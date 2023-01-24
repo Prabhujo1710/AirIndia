6fb24d52b87915e2655fec0190c454fce76a9755
Prabhujo1710 <prabhujo1710@gmail.com>
2023-01-24 10:41:47 +0530

    Create new

---
+++ src/test/java/org/com/Employee.java
@@ -0,0 +1,19 @@
+package org.com;
+
+public class Employee {
+	
+	private void comName() {
+	
+		System.out.println("Greens");
+	}
+	
+	private void comId() {
+
+		System.out.println("1234");
+	}
+	private void comPhoneNum() {
+
+System.out.println("8956877777");
+	}
+
+}
